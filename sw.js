6cdbbe8e785f5e0ceb605c66cbdb17c9db6cc329
Smims <smims00@gmail.com>
2026-03-17 17:16:33 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,40 @@
-self.addEventListener('fetch', function(event) {
-    event.respondWith(fetch(event.request));
+const CACHE_NAME = 'mycolab-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'favicon.ico',
+  'favicon-16x16.png',
+  'favicon-32x32.png',
+  'apple-touch-icon.png',
+  'android-chrome-192x192.png',
+  'android-chrome-512x512.png'
+];
+
+// Installation : Mise en cache des fichiers essentiels
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activation : Nettoyage des anciens caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Stratégie : Réseau d'abord, sinon Cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
